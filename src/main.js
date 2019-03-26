d2306bececbff4b21899e53a7f546e23f767e3ea
mrgao <gaogangwork@qq.com>
2019-03-26 11:50:58 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
-import App from "./App.vue"
-import "./plugins/iview.js"
+import App from "./App.vue";
+import "./plugins/iview.js";
+//为了兼容ie浏览器
+import "babel-polyfill";
 new Vue({
     el: "#app",
     render: h => h(App)
