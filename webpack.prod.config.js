94437d83b5600b0d389266fa9e5859287f3fdfc9
mrgao <gaogangwork@qq.com>
2019-03-26 15:38:00 +0800

    Update webpack.prod.config.js

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,8 +10,7 @@ var VueLoaderPlugin = require("vue-loader/lib/plugin");
 var webpackBaseConfig = require("./webpack.config.js");
 //清空构建目录
 var clearWebpack = require("clean-webpack-plugin");
-//代码压缩
-var UglifyESPlugin  = require("uglifyjs-webpack-plugin");
+
 
 webpackBaseConfig.plugins = [];
 module.exports = merge(webpackBaseConfig, {
@@ -38,26 +37,6 @@ module.exports = merge(webpackBaseConfig, {
         filename:"../index_pro.html",//此文件路径是相对于dist
     }),
     new VueLoaderPlugin(),//使用vue必须要加的哦
-    new UglifyESPlugin({//代码压缩
-        // 多嵌套了一层
-        uglifyOptions: {
-          compress: {
-            // 在UglifyJs删除没有用到的代码时不输出警告
-            warnings: false,
-            // 删除所有的 `console` 语句，可以兼容ie浏览器
-            drop_console: true,
-            // 内嵌定义了但是只用到一次的变量
-            collapse_vars: true,
-            // 提取出出现多次但是没有定义成变量去引用的静态值
-            reduce_vars: true,
-          },
-          output: {
-            // 最紧凑的输出
-            beautify: false,
-            // 删除所有的注释
-            comments: false,
-          }
-        }
-      })
+ 
   ]
 });
