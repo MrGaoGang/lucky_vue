2ff601c17490c2086531d5ae5a469bad6ab336d0
mrgao <gaogangwork@qq.com>
2019-04-02 17:16:20 +0800

    自动导入所有模块下的store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import cart from './modules/cart'
-import products from './modules/products'
-
+// 引入modules文件下所有模块的store
+import modules from '../modules'
 Vue.use(Vuex)
 
-
 export default new Vuex.Store({
-  modules: {
-    cart,
-    products
-  },
-  
+  modules: modules(require.context('./modules', true, /.+\.js$/))
 })
