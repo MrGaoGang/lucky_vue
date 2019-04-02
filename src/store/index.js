6bcbf5b99c978544f98245889a8c6f5122d16654
mrgao <gaogangwork@qq.com>
2019-04-02 17:21:27 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 // 引入modules文件下所有模块的store
 import modules from '../modules'
 Vue.use(Vuex)
-
+//require.context动态引入modules下的所有js文件
 export default new Vuex.Store({
   modules: modules(require.context('./modules', true, /.+\.js$/))
 })
