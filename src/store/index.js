0bf26d45d21770c2ea0905990ec71d8377b39c8e
mr.gao <gaogangwork@qq.com>
2019-04-02 19:12:30 +0800

    添加Strore模块自动导入

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 // 引入modules文件下所有模块的store
-import modules from '../modules'
+import modules from './modules'
 Vue.use(Vuex)
 //require.context动态引入modules下的所有js文件
 export default new Vuex.Store({
